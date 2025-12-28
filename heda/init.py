d3878094b37011dd1d8f00f53cdeb3acc003e9d8
Dinitha Wijewardhana <dini2040gh@gmail.com>
2025-12-28 22:07:45 +0530

    Wire gitignore template generation to cli

--- heda/init.py
+++ heda/init.py
@@ -1,8 +1,7 @@
 from pathlib import Path
-from heda.config import get_username
-from heda.utils.git_utils import git_add_commit, git_init
 from heda.templates.experiment_yaml import experiment_yaml_template
 from heda.templates.sample_code import sample_code_template
+from heda.templates.gitignore_template import python_gitignore_template
 
 def create_directory_structure(base_path: Path) -> None:
     
@@ -17,3 +16,5 @@ def create_template_files(base_path: Path, exp_name: object) -> None:
     experiment_yaml = experiment_yaml_template.format(exp_name=exp_name)
     (base_path / "experiment.yaml").write_text(experiment_yaml)
     (base_path / "src" / "main.py").write_text(sample_code_template)
+    (base_path / ".gitignore").write_text(python_gitignore_template)
+
